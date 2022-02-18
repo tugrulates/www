321a3988e789bbbde8185716a52c07fa31818d09
Tugrul Ates <me@tugrulates.com>
2022-02-18 15:00:12 -0800

    File permissions

--- fix-docs.sh
+++ fix-docs.sh
@@ -4,5 +4,6 @@ git add _docs
 git commit -m 'Fix submodules'
 git submodule add --force https://github.com/tugrulates/blog _docs
 git submodule update
+git submodule foreach git pull origin main
 git add _docs
 git commit -C HEAD --amend
