e17e049490bd636d13201648cfca59a96c5cd78a
Tugrul Ates <me@tugrulates.com>
2022-02-22 18:01:12 -0800

    Make html validation checks pass (#21)
    * Make html validation checks pass
    
    * Exclude node modules from git
    
    * Fix all invalid HTML sans kramdown output
    
    * Fix urls
    
    * Fix wrong feed links

---
+++ assets/js/math.js
@@ -0,0 +1,12 @@
+MathJax = {
+    options: {
+        enableMenu: false,
+    },
+    loader: { load: ["input/tex", "output/svg"] },
+    tex: {
+        inlineMath: [['$', '$'], ['\\(', '\\)']]
+    },
+    svg: {
+        fontCache: 'global'
+    }
+};
