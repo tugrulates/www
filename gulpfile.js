33a303b7a8782285ad6a7154ba98677fb83be757
Tugrul Ates <me@tugrulates.com>
2022-10-22 10:40:31 +0000

    Prettier for all

--- gulpfile.js
+++ gulpfile.js
@@ -2,215 +2,232 @@ import gulp from "gulp";
 import newer from "gulp-newer";
 import matter from "gulp-gray-matter";
 import addFrontMatter from "gulp-add-front-matter";
-import favicons from "gulp-favicons"
-import imagemin from "gulp-imagemin"
-import gm from "gulp-gm"
+import favicons from "gulp-favicons";
+import imagemin from "gulp-imagemin";
+import gm from "gulp-gm";
 
-import jpegtran from "imagemin-jpegtran"
-import mozjpeg from "imagemin-mozjpeg"
+import jpegtran from "imagemin-jpegtran";
+import mozjpeg from "imagemin-mozjpeg";
 
 import { spawn } from "child_process";
 import del from "delete";
 import path from "path";
-import minimist from "minimist"
+import minimist from "minimist";
 import through2 from "through2";
 
-
-var options = minimist(process.argv.slice(2),
-    {
-        boolean: "drafts",
-    });
-
+var options = minimist(process.argv.slice(2), {
+  boolean: "drafts",
+});
 
 // Convert markdown files into what zola will accept.
 //
 // This task exists because obsidian and zola have differences in opinion.
 const docs = {
-    input: "docs/posts/[!_]*.md",
-    output: "content",
-    delete: "content/[!_]*.md",
+  input: "docs/posts/[!_]*.md",
+  output: "content",
+  delete: "content/[!_]*.md",
 
-    wiki_link: /\[\[(?!\])((.*?))(?<!\[)\]\]/g,
-    internal_link: /\[(?!\])(.*?)(?<!\[)\]\((?!@\/)(.+?)\.md\)/g,
-    link_replace: (match, name, link) => { return `[${name}](@/${link.replace(" ", "%20")}.md)`; },
+  wiki_link: /\[\[(?!\])((.*?))(?<!\[)\]\]/g,
+  internal_link: /\[(?!\])(.*?)(?<!\[)\]\((?!@\/)(.+?)\.md\)/g,
+  link_replace: (match, name, link) => {
+    return `[${name}](@/${link.replace(" ", "%20")}.md)`;
+  },
 
-    scripts: {
-        math: /(\$(?=[^\s]).*(?<=[^\s])\$)|(\$\$.*\$\$)/,
-        graph: /^```mermaid/,
-    },
+  scripts: {
+    math: /(\$(?=[^\s]).*(?<=[^\s])\$)|(\$\$.*\$\$)/,
+    graph: /^```mermaid/,
+  },
 
-    build: () => gulp
-        .src(docs.input)
-        .pipe(matter())
-        .pipe(through2.obj((file, _, cb) => {
-            var blocks = file.contents.toString()
-                .trim()
-                .split("\n\n")
-                .map(x => x.trim());
-            const data = file.data;
+  build: () =>
+    gulp
+      .src(docs.input)
+      .pipe(matter())
+      .pipe(
+        through2.obj((file, _, cb) => {
+          var blocks = file.contents
+            .toString()
+            .trim()
+            .split("\n\n")
+            .map((x) => x.trim());
+          const data = file.data;
 
-            // Docs are draft by default.
-            data.draft = data.state != 'public';
+          // Docs are draft by default.
+          data.draft = data.state != "public";
 
-            // Generate title from file name.
-            if (!data.title) {
-                data.title = path.parse(file.path).name;
+          // Generate title from file name.
+          if (!data.title) {
+            data.title = path.parse(file.path).name;
+          }
+
+          // Mark end of first paragraph to generate summary.
+          if (blocks.length > 0) {
+            blocks[0] += "<!-- more -->";
+          }
+
+          // Copy "tags" into "taxonomies.tags".
+          // if (data.tags) {
+          //     data.taxonomies = { tags: data.tags.split(" ") };
+          // }
+
+          // Update internal links.
+          blocks = blocks.map((x) =>
+            x
+              .replaceAll(docs.wiki_link, docs.link_replace)
+              .replaceAll(docs.internal_link, docs.link_replace)
+          );
+
+          // Determine which js to load based on content.
+          var scripts = [];
+          for (const script in docs.scripts) {
+            if (blocks.find((x) => docs.scripts[script].test(x))) {
+              scripts.push(script);
             }
+          }
 
-            // Mark end of first paragraph to generate summary.
-            if (blocks.length > 0) {
-                blocks[0] += '<!-- more -->';
-            }
+          // Create extras with defaults.
+          data.extra = {
+            scripts: scripts,
+            cover: data.cover || "/photos/roundest-object-on-earth.jpg",
+          };
 
-            // Copy "tags" into "taxonomies.tags".
-            // if (data.tags) {
-            //     data.taxonomies = { tags: data.tags.split(" ") };
-            // }
+          file.contents = Buffer.from(blocks.join("\n\n"));
+          cb(null, file);
+        })
+      )
+      .pipe(addFrontMatter())
+      .pipe(gulp.dest(docs.output)),
 
-            // Update internal links.
-            blocks = blocks.map(
-                x => x
-                    .replaceAll(docs.wiki_link, docs.link_replace)
-                    .replaceAll(docs.internal_link, docs.link_replace));
-
-            // Determine which js to load based on content.
-            var scripts = [];
-            for (const script in docs.scripts) {
-                if (blocks.find(x => docs.scripts[script].test(x))) {
-                    scripts.push(script);
-                }
-            }
-
-            // Create extras with defaults.
-            data.extra = {
-                scripts: scripts,
-                cover: data.cover || "/photos/roundest-object-on-earth.jpg"
-            };
-
-            file.contents = Buffer.from(blocks.join("\n\n"))
-            cb(null, file);
-        }))
-        .pipe(addFrontMatter())
-        .pipe(gulp.dest(docs.output)),
-
-    watch: () => gulp.watch(docs.input, docs.build),
-    clean: () => del(docs.delete),
+  watch: () => gulp.watch(docs.input, docs.build),
+  clean: () => del(docs.delete),
 };
-
 
 // Copy post photos.
 const photos = {
-    input: "docs/photos/*.jpg",
-    output: "static/photos",
-    delete: "static/photos",
+  input: "docs/photos/*.jpg",
+  output: "static/photos",
+  delete: "static/photos",
 
-    build: () => gulp
-        .src(photos.input)
-        .pipe(imagemin([jpegtran()]))
-        .pipe(gm(function (gmfile) {
-            return gmfile.colorspace('Rec709Luma').resize(3200);
-        }))
-        .pipe(imagemin([mozjpeg({ quality: 75, progressive: true })]))
-        .pipe(gulp.dest(photos.output)),
+  build: () =>
+    gulp
+      .src(photos.input)
+      .pipe(imagemin([jpegtran()]))
+      .pipe(
+        gm(function (gmfile) {
+          return gmfile.colorspace("Rec709Luma").resize(3200);
+        })
+      )
+      .pipe(imagemin([mozjpeg({ quality: 75, progressive: true })]))
+      .pipe(gulp.dest(photos.output)),
 
-    watch: () => gulp.watch(photos.input, photos.build),
-    clean: () => del(photos.delete),
+  watch: () => gulp.watch(photos.input, photos.build),
+  clean: () => del(photos.delete),
 };
-
 
 // Copy post files.
 const files = {
-    input: "docs/files/**/*",
-    output: "static",
-    delete: ["static/*",
-        "!static/js",
-        "!static/png",
-        "!static/svg",
-        "!static/photos",
-        "!static/processed_images"],
+  input: "docs/files/**/*",
+  output: "static",
+  delete: [
+    "static/*",
+    "!static/js",
+    "!static/png",
+    "!static/svg",
+    "!static/photos",
+    "!static/processed_images",
+  ],
 
-    build: () => gulp
-        .src(files.input)
-        .pipe(gulp.dest(files.output)),
+  build: () => gulp.src(files.input).pipe(gulp.dest(files.output)),
 
-    watch: () => gulp.watch(files.input, files.build),
-    clean: () => del(files.delete),
+  watch: () => gulp.watch(files.input, files.build),
+  clean: () => del(files.delete),
 };
-
 
 // Generate favicon and manifest files.
 const favicon = {
-    author: "Tugrul Ates",
-    url: "tugrul.blog",
+  author: "Tugrul Ates",
+  url: "tugrul.blog",
 
-    input: "static/png/me.png",
-    output: "static",
-    html: "templates/favicon.html",
-    delete: ["static/*.*", "templates/favicon.html"],
+  input: "static/png/me.png",
+  output: "static",
+  html: "templates/favicon.html",
+  delete: ["static/*.*", "templates/favicon.html"],
 
-    build: () => gulp.src(favicon.input)
-        .pipe(newer(favicon.html))
-        .pipe(favicons({
-            appName: favicon.url,
-            appShortName: favicon.url,
-            appDescription: "Let’s see where this goes",
-            version: null,
-            developerName: favicon.author,
-            developerURL: `https://${favicon.url}/`,
-            url: `https://${favicon.url}/`,
-            scope: "/",
-            background: "#2b2622",
-            theme_color: "#2b2622",
-            html: path.relative(favicon.output, favicon.html),
-            pipeHTML: true,
-            replace: true,
-        }))
-        .pipe(gulp.dest(favicon.output)),
+  build: () =>
+    gulp
+      .src(favicon.input)
+      .pipe(newer(favicon.html))
+      .pipe(
+        favicons({
+          appName: favicon.url,
+          appShortName: favicon.url,
+          appDescription: "Let’s see where this goes",
+          version: null,
+          developerName: favicon.author,
+          developerURL: `https://${favicon.url}/`,
+          url: `https://${favicon.url}/`,
+          scope: "/",
+          background: "#2b2622",
+          theme_color: "#2b2622",
+          html: path.relative(favicon.output, favicon.html),
+          pipeHTML: true,
+          replace: true,
+        })
+      )
+      .pipe(gulp.dest(favicon.output)),
 
-    watch: () => gulp.watch(favicon.input, favicon.build),
-    clean: () => del(favicon.delete, favicon.html),
+  watch: () => gulp.watch(favicon.input, favicon.build),
+  clean: () => del(favicon.delete, favicon.html),
 };
-
 
 // Generate public folder with zola.
 const site = {
-    config: "config.toml",
-    input: ["config.toml", "content/**", "static/**", "templates/**"],
-    delete: ["public", "static/processed_photos"],
+  config: "config.toml",
+  input: ["config.toml", "content/**", "static/**", "templates/**"],
+  delete: ["public", "static/processed_photos"],
 
-    command: (command) => spawn(
-        "zola",
-        [command].concat(options.drafts ? ["--drafts"] : []),
-        { stdio: "inherit" }),
+  command: (command) =>
+    spawn("zola", [command].concat(options.drafts ? ["--drafts"] : []), {
+      stdio: "inherit",
+    }),
 
-    build: () => site.command("build"),
-    check: () => site.command("check"),
-    serve: () => site.command("serve"),
-    watch: () => gulp.watch(site.input, site.build),
-    clean: () => del(site.delete),
+  build: () => site.command("build"),
+  check: () => site.command("check"),
+  serve: () => site.command("serve"),
+  watch: () => gulp.watch(site.input, site.build),
+  clean: () => del(site.delete),
 };
-
 
 const objs = { docs, photos, files, favicon, site };
 for (const obj in objs) {
-    for (const attr in objs[obj]) {
-        if (objs[obj][attr] instanceof Function && objs[obj][attr].length == 0) {
-            objs[obj][attr].displayName = `${obj}.${attr}`;
-            gulp.task(objs[obj][attr]);
-        }
+  for (const attr in objs[obj]) {
+    if (objs[obj][attr] instanceof Function && objs[obj][attr].length == 0) {
+      objs[obj][attr].displayName = `${obj}.${attr}`;
+      gulp.task(objs[obj][attr]);
     }
+  }
 }
 
 export const build = gulp.series(
-    gulp.parallel(docs.build, photos.build, files.build, favicon.build),
-    site.build);
+  gulp.parallel(docs.build, photos.build, files.build, favicon.build),
+  site.build
+);
 
 export const dev = gulp.parallel(
-    docs.watch, photos.watch, favicon.watch, files.watch, site.serve);
+  docs.watch,
+  photos.watch,
+  favicon.watch,
+  files.watch,
+  site.serve
+);
 
 export const check = gulp.series(build, site.check);
 
 export const clean = gulp.parallel(
-    docs.clean, photos.clean, files.clean, favicon.clean, site.clean);
+  docs.clean,
+  photos.clean,
+  files.clean,
+  favicon.clean,
+  site.clean
+);
 
 export default build;
--- static/js/theme.js
+++ static/js/theme.js
@@ -1,6 +1,8 @@
 function css(property, value = null) {
   if (value === null) {
-    return window.getComputedStyle(document.documentElement).getPropertyValue(property);
+    return window
+      .getComputedStyle(document.documentElement)
+      .getPropertyValue(property);
   }
   document.documentElement.style.setProperty(property, value);
 }
@@ -36,14 +38,17 @@ function initializeTheme() {
   }
   dataTheme(
     pref.theme ||
-    ((window.matchMedia("(prefers-color-scheme: light)").matches) ? "light" : "dark"));
+      (window.matchMedia("(prefers-color-scheme: light)").matches
+        ? "light"
+        : "dark")
+  );
 }
 
 function switchLight() {
   const theme = css("--light") > 0.5 ? "dark" : "light";
   dataTheme(theme);
   css("--light", "");
-  prefs({ "theme": theme, "light": null });
+  prefs({ theme: theme, light: null });
   updateLightIcon();
   updateInputs();
   // TODO: #13 Switch graph theme with page
@@ -55,7 +60,7 @@ function switchLight() {
 function randomizeHue() {
   const hue = Math.floor(Math.random() * 360);
   css("--hue", hue);
-  prefs({ "hue": hue });
+  prefs({ hue: hue });
 }
 
 function switchHue() {
@@ -68,25 +73,28 @@ function changeLight() {
   const theme = light > 0.5 ? "light" : "dark";
   dataTheme(theme);
   css("--light", light);
-  prefs({ "light": light, "theme": null });
+  prefs({ light: light, theme: null });
   updateLightIcon();
 }
 
 function changeHue() {
   const hue = $("#hue-tune").val();
   css("--hue", hue);
-  prefs({ "hue": hue });
+  prefs({ hue: hue });
 }
 
 function updateLightIcon() {
   const light = parseFloat(css("--light"));
-  const icon = (
-    light < 0.25 ? "darker" :
-      (light < 0.5 ? "dark" :
-        (light < 0.75 ? "lighter" : "light")));
+  const icon =
+    light < 0.25
+      ? "darker"
+      : light < 0.5
+      ? "dark"
+      : light < 0.75
+      ? "lighter"
+      : "light";
   $("#light-icon").attr("xlink:href", `/svg/icons.svg#${icon}`);
 }
-
 
 function updateInputs() {
   $("#light-tune").val(parseFloat($("html").css("--light")));
@@ -94,8 +102,9 @@ function updateInputs() {
 }
 
 function createControl(controls, control) {
-  controls.append($(
-    `<div class="${control.name} control">
+  controls.append(
+    $(
+      `<div class="${control.name} control">
       <label for="${control.name}-tune">change ${control.name}</label>
       <input class="tune" id="${control.name}-tune" type="range"
              min=${control.min} max=${control.max} step=${control.step}>
@@ -105,39 +114,44 @@ function createControl(controls, control) {
           <use id="${control.name}-icon" xlink:href=/svg/icons.svg#${control.name}></use>
         </svg>
       </button>
-    </div>`));
+    </div>`
+    )
+  );
   $(`#${control.name}-switch`).click(function () {
     control.switch();
     if ($(`#${control.name}-switch`).is(":hover")) {
       $(`#${control.name}-switch`).blur();
     }
   });
-  $(`#${control.name}-tune`).on('input', control.change);
+  $(`#${control.name}-tune`).on("input", control.change);
 }
 
 initializeTheme();
-document.addEventListener('DOMContentLoaded', function () {
-  const controls = $("<div class='controls'></div>");
-  $(".control-box").append(controls);
-  createControl(controls, {
-    name: "light",
-    aria: "toggle theme",
-    switch: switchLight,
-    change: changeLight,
-    min: 0,
-    max: 1,
-    step: 0.01,
-  });
-  createControl(controls, {
-    name: "hue",
-    aria: "random color",
-    switch: switchHue,
-    change: changeHue,
-    min: 0,
-    max: 359,
-    step: 1,
-  });
-  updateInputs();
-  updateLightIcon();
-}, false);
-
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    const controls = $("<div class='controls'></div>");
+    $(".control-box").append(controls);
+    createControl(controls, {
+      name: "light",
+      aria: "toggle theme",
+      switch: switchLight,
+      change: changeLight,
+      min: 0,
+      max: 1,
+      step: 0.01,
+    });
+    createControl(controls, {
+      name: "hue",
+      aria: "random color",
+      switch: switchHue,
+      change: changeHue,
+      min: 0,
+      max: 359,
+      step: 1,
+    });
+    updateInputs();
+    updateLightIcon();
+  },
+  false
+);
