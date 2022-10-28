72eb00d1154c517cf40191e139134e29f497d58d
Tugrul Ates <me@tugrulates.com>
2022-10-28 18:02:55 +0000

    Make buttons 48px
    Fixes #75
    
    Buttons were 46px before. This passes the tap target test in
    Lighthouse (#74).

--- tests/lighthouserc.live.cjs
+++ tests/lighthouserc.live.cjs
@@ -13,8 +13,7 @@ module.exports = {
         "categories:performance": ["error", { minScore: 0.9 }],
         "categories:accessibility": ["error", { minScore: 1 }],
         "categories:best-practices": ["error", { minScore: 1 }],
-        // TODO: #75 Fix tap target sizes
-        "categories:seo": ["error", { minScore: 0.95 }],
+        "categories:seo": ["error", { minScore: 1 }],
       },
     },
     upload: {
