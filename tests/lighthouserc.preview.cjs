be2ac0e0a71fcd8651ab6243c4d943e9b1443d12
Tugrul Ates <me@tugrulates.com>
2022-10-31 09:47:50 +0300

    Run Lighthouse with the Netlify plugin (#113)

--- tests/lighthouserc.preview.cjs
+++
@@ -1,5 +0,0 @@
-const config = require("./lighthouserc.live.cjs");
-config.ci.collect.settings.skipAudits.push(
-  "is-crawlable" // preview deploys are not crawlable by design
-);
-module.exports = config;
