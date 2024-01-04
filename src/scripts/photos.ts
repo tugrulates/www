31405829a24dda44e6fdc09dcc298c7e4b056aa2
Tugrul Ates <me@tugrulates.com>
2024-01-04 11:17:50 +0000

    photo detail page

--- src/scripts/photos.ts
+++ src/scripts/photos.ts
@@ -3,7 +3,8 @@ import fs from "fs";
 import { glob } from "glob";
 import { ExifDateTime, exiftool } from "exiftool-vendored";
 
-const DIR = "src/content/photos";
+const IMAGES_DIR = "src/images/photos";
+const CONTENT_DIR = "src/content/photos";
 
 interface PhotoData {
   cover: string;
@@ -18,12 +19,12 @@ interface PhotoData {
 }
 
 async function extractMetadata(photo: string) {
-  const cover = `${DIR}/${photo}/cover.jpg`;
+  const cover = `${IMAGES_DIR}/${photo}/cover.jpg`;
   const tags = await exiftool.read(cover);
-  const file = `./${photo}/${tags.FileName ?? ""}`;
+  const file = `/${photo}/${tags.FileName ?? ""}`;
   const data: PhotoData = {
-    cover: file,
-    square: file.replace("/cover.jpg", "/square.jpg"),
+    cover: `../../images/photos/${photo}/cover.jpg`,
+    square: `../../images/photos/${photo}/square.jpg`,
     title: tags.Headline ?? "",
     description: tags.ImageDescription ?? "",
     date:
@@ -36,16 +37,17 @@ async function extractMetadata(photo: string) {
     country: tags.Country ?? "",
   };
   const json = JSON.stringify(data, null, 2);
-  console.log(tags);
-  console.log(json);
-  fs.writeFileSync(`${DIR}/${photo}.json`, json);
+  fs.writeFileSync(`${CONTENT_DIR}/${photo}.json`, json);
 }
 
-const covers = glob.sync(`${DIR}/*/cover.jpg`);
+const covers = glob.sync(`${IMAGES_DIR}/*/cover.jpg`);
 const photos = covers.map((cover) => {
   const photo = path.dirname(cover).split("/").pop();
   return photo;
 });
+if (!fs.existsSync(CONTENT_DIR)) {
+  fs.mkdirSync(CONTENT_DIR);
+}
 Promise.all(
   photos.map((photo) => {
     if (photo) {
