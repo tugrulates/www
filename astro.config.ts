2fe3b4e8ebb1a0dde6d90a795f4b818bbd438ac4
Tugrul Ates <me@tugrulates.com>
2024-01-22 22:07:29 +0000

    slashed test

--- astro.config.ts
+++ astro.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
         {
           userAgent: "*",
           allow: "/",
-          disallow: "/test",
+          disallow: "/test/",
         },
       ],
     }),
