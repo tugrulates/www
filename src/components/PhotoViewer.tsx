8c64a66b267dd481549e1d07139ff0652922bf37
Tugrul Ates <me@tugrulates.com>
2024-01-26 06:13:17 +0000

    cover

--- src/components/PhotoViewer.tsx
+++ src/components/PhotoViewer.tsx
@@ -2,8 +2,9 @@ import { Dialog } from "@headlessui/react";
 import { useState } from "react";
 
 interface Props {
-  button?: JSX.Element;
   image?: JSX.Element;
+  openButton?: JSX.Element;
+  closeButton?: JSX.Element;
 }
 
 export default function PhotoViewer(props: Props): JSX.Element {
@@ -18,13 +19,25 @@ export default function PhotoViewer(props: Props): JSX.Element {
         setIsOpen(false);
       }}
     >
+      <Dialog.Title className="sr-only">Viewing image</Dialog.Title>
       <Dialog.Panel
         onClick={() => {
           setIsOpen(false);
         }}
-        className="fixed inset-0 flex h-screen w-screen transform items-center justify-center overflow-hidden overflow-y-auto bg-stone-300 align-middle dark:bg-stone-700"
+        className="fixed inset-0 flex h-screen w-screen transform items-center justify-center overflow-hidden overflow-y-auto bg-gradient-to-b from-stone-400 to-stone-300 align-middle dark:from-stone-600 dark:to-stone-700 "
       >
-        <div className="h-fit w-fit bg-stone-200/20">{props.image}</div>
+        <div className="group/cover h-fit w-fit bg-stone-200/20">
+          {props.image}
+          <button
+            aria-label="Close image"
+            onClick={() => {
+              setIsOpen(true);
+            }}
+            className="group absolute right-2 top-2"
+          >
+            {props.closeButton}
+          </button>
+        </div>
       </Dialog.Panel>
     </Dialog>
   ) : (
@@ -35,7 +48,7 @@ export default function PhotoViewer(props: Props): JSX.Element {
       }}
       className="group"
     >
-      {props.button}
+      {props.openButton}
     </button>
   );
 }
