5202cbd629165052a8d8c153dfe3113092e64d55
Tugrul Ates <me@tugrulates.com>
2024-02-09 08:31:07 +0000

    fix

--- src/url.ts
+++ src/url.ts
@@ -1,7 +1,9 @@
+import { SITE } from "./consts";
+
 export function getChildUrl(url: URL, path: string): string {
   return `${url.pathname}/${path}`.replace("//", "/");
 }
 
 export function getFullChildUrl(url: URL, path: string): string {
-  return `${url.href}/${path}`.replace(/[^:]\/\//, "/");
+  return `${SITE.url}/${path}`.replace(/[^:]\/\//, "/");
 }
