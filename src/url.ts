4ce1194d151a65a16fa356a27e8c25dc62d99c91
Tugrul Ates <me@tugrulates.com>
2024-02-09 08:48:50 +0000

    rename

--- src/url.ts
+++ src/url.ts
@@ -4,6 +4,6 @@ export function getChildUrl(url: URL, path: string): string {
   return `${url.pathname}/${path}`.replace("//", "/");
 }
 
-export function getFullChildUrl(url: URL, path: string): string {
+export function getCanonicalChildUrl(url: URL, path: string): string {
   return `${SITE.url}${getChildUrl(url, path)}`;
 }
