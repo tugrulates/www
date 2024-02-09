306ae8a81f61f5811ccc894162bf7e660b115d38
Tugrul Ates <me@tugrulates.com>
2024-02-09 14:07:21 +0000

    iterate

--- src/components/OpenGraphImage.tsx
+++ src/components/OpenGraphImage.tsx
@@ -18,20 +18,20 @@ export function OpenGraphImage({
   description,
 }: Props): JSX.Element {
   return (
-    <div tw="relative flex flex-row w-full h-full bg-black">
+    <div tw="relative flex flex-row w-full h-full bg-black p-16">
       <div tw="absolute inset-0 flex">
         <img
           src={background}
           width={DIMENSIONS.opengraph_source_width}
           height={DIMENSIONS.opengraph_source_height}
-          style={{ objectFit: "cover", filter: "blur(20px) brightness(40%)" }}
+          style={{ objectFit: "cover", filter: "blur(40px) brightness(40%)" }}
         />
       </div>
       <div
-        tw="flex-1 text-5xl px-16 py-24 flex flex-col items-center justify-between text-white"
+        tw="flex-1 text-5xl pr-16 py-8 flex flex-col items-center justify-between text-white"
         style={{ fontFamily: "Regular" }}
       >
-        <div tw="p-2 rounded-full text-black bg-stone-200 shadow-lg shadow-stone-200/30 flex items-center">
+        <div tw="p-2 rounded-full text-black bg-stone-200 shadow-lg shadow-stone-900/80 flex items-center">
           <img src={avatar} tw={"w-24 h-24 rounded-full border border-black"} />
           <span tw="px-8">{SITE.domain}</span>
         </div>
@@ -39,15 +39,18 @@ export function OpenGraphImage({
           {title}
         </h1>
         <div tw="text-center">{description}</div>
-        <div tw="text-black px-16 py-8 rounded-2xl bg-indigo-200 shadow-lg shadow-indigo-200/30">
+        <div tw="text-black px-16 py-8 rounded-2xl bg-indigo-200 shadow-lg shadow-indigo-900/80">
           {cta}
         </div>
       </div>
       <img
+        tw="shadow-lg shadow-stone-900/80"
         src={background}
-        width={DIMENSIONS.opengraph_source_height}
-        height={DIMENSIONS.opengraph_source_height}
-        style={{ objectFit: "cover" }}
+        width={DIMENSIONS.opengraph_source_height - 128}
+        height={DIMENSIONS.opengraph_source_height - 128}
+        style={{
+          borderRadius: "5%",
+        }}
       />
     </div>
   );
