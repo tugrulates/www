b9936590ab286dfc69b3d0eaddc287cb523c8030
Tugrul Ates <me@tugrulates.com>
2024-12-10 17:39:34 +0100

    Less hacky URL manipulation (#431)
    Fixes #427

--- src/url.ts
+++ src/url.ts
@@ -1,9 +1,9 @@
 import { SITE } from "~/config";
 
 export function getChildUrl(url: URL, path: string): string {
-  return `${url.pathname}/${path}`.replace("//", "/");
+  return new URL(path, url).pathname;
 }
 
 export function getCanonicalChildUrl(url: URL, path: string): string {
-  return `${SITE.url}${getChildUrl(url, path)}`.replace(/\/\//, "/");
+  return new URL(getChildUrl(url, path), SITE.url).toString();
 }
