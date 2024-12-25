1bf7d0c9aac874293ef0027ef6a8195785729056
Tugrul Ates <me@tugrulates.com>
2024-12-25 11:28:36 +0100

    debug

--- src/pages/og.jpg.ts
+++ src/pages/og.jpg.ts
@@ -5,8 +5,11 @@ export const prerender = false;
 
 async function getMetadata(path: string): Promise<string | null> {
   try {
-    return JSON.parse(await readFile(join(path, "metadata.json"), "utf-8"));
+    const metadata = await readFile(join(path, "metadata.json"), "utf-8");
+    console.log("Found at", path);
+    return JSON.parse(metadata);
   } catch {
+    console.log("Not found at", path);
     return null;
   }
 }
@@ -18,7 +21,15 @@ interface Input {
 export async function GET({ request }: Input): Promise<Response> {
   const path = new URL(request.url).searchParams.get("path") ?? "";
   const metadata = (await getMetadata(path)) ??
-    (await getMetadata(join("dist/client", path)));
+    (await getMetadata(join("dist/client", path))) ??
+    (await getMetadata(join("client", path))) ??
+    (await getMetadata(join("../client", path))) ??
+    (await getMetadata(join("server", path))) ??
+    (await getMetadata(join("../server", path))) ??
+    (await getMetadata(join("static/server", path))) ??
+    (await getMetadata(join("static/client", path))) ??
+    (await getMetadata(join("../static/server", path))) ??
+    (await getMetadata(join("../static/client", path)));
   if (!metadata) return new Response("Not found", { status: 404 });
   // return await getOpenGraphImage({
   //   title: "Tugrul Ates",
