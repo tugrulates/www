7f16558cd5f959e4a9d53bca3eb173c9c2231c47
Tugrul Ates <me@tugrulates.com>
2024-12-27 12:46:03 +0100

    Add headers with middleware

---
+++ src/middleware.ts
@@ -0,0 +1,38 @@
+import { APIContext, MiddlewareNext } from "astro";
+import { sequence } from "~/site.astro";
+
+const HEADERS = [
+  {
+    source: /\/(.*)/,
+    headers: {
+      "Content-Security-Policy":
+        "default-src 'none'; connect-src 'self' https://vercel.live https://*.pusher.com wss://*.pusher.com; font-src 'self' https://vercel.live https://*.vercel.com https://fonts.gstatic.com; frame-src 'self' https://vercel.live https://www.youtube-nocookie.com; img-src 'self' data: https://vercel.com https://i.ytimg.com; manifest-src 'self'; script-src 'self' 'unsafe-inline' https://vercel.live; style-src 'self' 'unsafe-inline' https://vercel.live; base-uri 'none'; form-action 'none'; frame-ancestors 'none';",
+      "Referrer-Policy": "strict-origin-when-cross-origin",
+      "X-Content-Type-Options": "nosniff",
+      "X-Frame-Options": "DENY",
+      "X-XSS-Protection": "1; mode=block",
+    },
+  },
+  {
+    source: /\/_astro\/(.*)/,
+    headers: {
+      "Cache-Control": "public, max-age=0, s-maxage=0",
+    },
+  },
+];
+
+async function addHeaders(context: APIContext, next: MiddlewareNext) {
+  const response = await next();
+  const headers = new Headers(response.headers);
+  const path = new URL(context.url).pathname;
+  for (const { source, headers: header } of HEADERS) {
+    if (source.test(path)) {
+      for (const [key, value] of Object.entries(header)) {
+        headers.set(key, value);
+      }
+    }
+  }
+  return new Response(response.body, { ...response, headers });
+}
+
+export const onRequest = sequence(addHeaders);
