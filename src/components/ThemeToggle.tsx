a9acaf8fa6217a970e822d6a2a140e779e31428e
Tugrul Ates <me@tugrulates.com>
2026-04-04 14:09:28 +0200

    Update Tailwind to v4

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -7,54 +7,49 @@ interface Props {
 }
 
 interface State {
-  dark: boolean;
+  theme: string;
 }
 
 function toggleTheme(
-  theme: State,
-  setTheme: Dispatch<SetStateAction<State>>,
+  state: State,
+  setState: Dispatch<SetStateAction<State>>,
 ): void {
-  const newTheme = { dark: !theme.dark };
-  setTheme(newTheme);
-  localStorage.setItem("theme", JSON.stringify(newTheme));
+  const toggled = { theme: state.theme === "dark" ? "light" : "dark" };
+  setState(toggled);
+  localStorage.setItem("theme", JSON.stringify(toggled));
 }
 
 export default function ThemeToggle(
   { lightIcon, darkIcon }: Props,
 ): React.JSX.Element {
-  const [theme, setTheme] = useState({ dark: true });
+  const [state, setState] = useState({ theme: "dark" });
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
     if (!isMounted) {
-      setTheme({ dark: document.documentElement.classList.contains("dark") });
+      setState({
+        theme: document.documentElement.getAttribute("data-theme") ?? "light",
+      });
       setIsMounted(true);
       return;
     }
-    if (theme.dark) {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-  }, [theme]);
+    document.documentElement.setAttribute("data-theme", state.theme);
+  }, [state]);
 
   if (!isMounted) return <div className="h-12 w-12"></div>;
 
   return (
-    <Field className="group h-12 w-12 transition-transform">
+    <Field className="group z-0 h-12 w-12 transition-transform">
       <Label className="sr-only">Switch theme</Label>
       <Switch
-        checked={theme.dark}
-        onChange={() => {
-          toggleTheme(theme, setTheme);
-        }}
+        checked={state.theme === "dark"}
+        onChange={() => toggleTheme(state, setState)}
+        className={[
+          "group cursor-pointer h-24 w-24 -translate-x-12 transition-transform duration-1000",
+          state.theme === "dark" ? "rotate-180" : "rotate-0",
+        ].join(" ")}
       >
-        <div
-          className={[
-            "relative z-0 flex h-24 w-24 -translate-x-12 flex-col overflow-hidden transition-transform duration-1000",
-            theme.dark ? "rotate-180" : "rotate-0",
-          ].join(" ")}
-        >
+        <div className="relative flex flex-col overflow-hidden">
           <div className="h-12 w-12 self-end p-2 group-hover:scale-110">
             {lightIcon}
           </div>
