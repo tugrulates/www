14b4055a88b577322f7f937eaae2e70f30578a14
Tugrul Ates <me@tugrulates.com>
2026-04-05 13:13:37 +0200

    Do not link to posts from about

--- src/pages/og.jpg.ts
+++ src/pages/og.jpg.ts
@@ -6,14 +6,12 @@ export async function GET() {
   const about = await getEntry("pages", "about");
   if (!about) throw new Error("About page not found");
   const cover = await getCover(about.data.cover);
-  return await getOpenGraphImage(
-    {
-      site: SITE.url,
-      image: cover.data.wide,
-      title: SITE.title,
-      subtitle: SITE.description,
-      description: about?.data.description,
-      cta: "Visit",
-    },
-  );
+  return await getOpenGraphImage({
+    site: SITE.url,
+    image: cover.data.wide,
+    title: SITE.title,
+    subtitle: SITE.description,
+    description: about?.data.description,
+    cta: "Visit",
+  });
 }
